f7426516f9763266a5572e2851f10fa1b09e4375
Abdurrahman al djufry <Abdurrahmanaldjufry@gmail.com>
2023-12-04 15:48:59 +0700

    interval card update

--- src/ab-component/Reviewsec.jsx
+++ src/ab-component/Reviewsec.jsx
@@ -24,83 +24,87 @@ const ReviewSec = () => {
   }, []);
   return (
     <div ref={componentRef}>
-      <Carousel
-        controls={false}
-        indicators={isBelow730 ? false : true}
-        variant="dark"
-        style={{ minHeight: "350px", width: "100%", marginBottom: "20px" }}
-        className={
-          isVisible ? "caraousel-rev animation-fade-left" : "before-animation"
-        }
-      >
-        <Carousel.Item interval={5000}>
-          <Card
-            style={{ width: "100%", height: "300px" }}
-            className="card-style"
-          >
-            <Card.Img
-              variant="left"
-              src="https://i.ibb.co/0Jv0WP5/ezgif-com-gif-maker.webp"
-              style={{ maxWidth: "300px", maxHeight: "300px" }}
-            />
-            <Card.Body>
-              <Card.Title className="fredokaB">Mr.wong</Card.Title>
-              <Card.Text className="fredokaB">
-                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Itaque
-                officia recusandae sunt sapiente minus aliquam. A ipsam placeat,
-                nulla laboriosam sit repellendus ipsa natus fuga blanditiis
-                recusandae, doloremque atque deserunt?
-              </Card.Text>
-              <Button variant="primary">Go somewhere</Button>
-            </Card.Body>
-          </Card>
-        </Carousel.Item>
-        <Carousel.Item interval={5000}>
-          <Card
-            style={{ width: "100%", height: "300px" }}
-            className="card-style"
-          >
-            <Card.Img
-              variant="left"
-              src="https://i.ibb.co/0Jv0WP5/ezgif-com-gif-maker.webp"
-              style={{ maxWidth: "300px", maxHeight: "300px" }}
-            />
-            <Card.Body>
-              <Card.Title className="fredokaB">Card Title</Card.Title>
-              <Card.Text className="fredokaB">
-                Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-                Reprehenderit eligendi provident consequuntur quaerat, facere
-                adipisci exercitationem recusandae laboriosam porro magnam sit
-                laudantium repudiandae voluptatibus ducimus totam dignissimos
-                ratione quidem! Et!
-              </Card.Text>
-              <Button variant="primary">Go somewhere</Button>
-            </Card.Body>
-          </Card>
-        </Carousel.Item>{" "}
-        <Carousel.Item interval={5000}>
-          <Card
-            style={{ width: "100%", height: "300px" }}
-            className="card-style"
-          >
-            <Card.Img
-              variant="left"
-              src="https://i.ibb.co/0Jv0WP5/ezgif-com-gif-maker.webp"
-              style={{ maxWidth: "300px", maxHeight: "300px" }}
-            />
-            <Card.Body>
-              <Card.Title className="fredokaB">Card Title</Card.Title>
-              <Card.Text className="fredokaB">
-                Lorem ipsum dolor sit amet consectetur, adipisicing elit.
-                Dolores atque quos nostrum necessitatibus ipsam rem inventore
-                reiciendis, distinctio eveniet esse quasi id aspernatur fugit
-                dignissimos laboriosam quibusdam vel voluptates? Blanditiis.
-              </Card.Text>
-              <Button variant="primary">Go somewhere</Button>
-            </Card.Body>
-          </Card>
-        </Carousel.Item>
-      </Carousel>
+      {isVisible ? (
+        <Carousel
+          controls={false}
+          indicators={isBelow730 ? false : true}
+          variant="dark"
+          style={{ minHeight: "350px", width: "100%", marginBottom: "20px" }}
+          className={
+            isVisible ? "caraousel-rev animation-fade-left" : "before-animation"
+          }
+        >
+          <Carousel.Item interval={3500}>
+            <Card
+              style={{ width: "100%", height: "300px" }}
+              className="card-style"
+            >
+              <Card.Img
+                variant="left"
+                src="https://i.ibb.co/0Jv0WP5/ezgif-com-gif-maker.webp"
+                style={{ maxWidth: "300px", maxHeight: "300px" }}
+              />
+              <Card.Body>
+                <Card.Title className="fredokaB">Mr.wong</Card.Title>
+                <Card.Text className="fredokaB">
+                  Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+                  Itaque officia recusandae sunt sapiente minus aliquam. A ipsam
+                  placeat, nulla laboriosam sit repellendus ipsa natus fuga
+                  blanditiis recusandae, doloremque atque deserunt?
+                </Card.Text>
+                <Button variant="primary">Go somewhere</Button>
+              </Card.Body>
+            </Card>
+          </Carousel.Item>
+          <Carousel.Item interval={2000}>
+            <Card
+              style={{ width: "100%", height: "300px" }}
+              className="card-style"
+            >
+              <Card.Img
+                variant="left"
+                src="https://i.ibb.co/0Jv0WP5/ezgif-com-gif-maker.webp"
+                style={{ maxWidth: "300px", maxHeight: "300px" }}
+              />
+              <Card.Body>
+                <Card.Title className="fredokaB">Card Title</Card.Title>
+                <Card.Text className="fredokaB">
+                  Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+                  Reprehenderit eligendi provident consequuntur quaerat, facere
+                  adipisci exercitationem recusandae laboriosam porro magnam sit
+                  laudantium repudiandae voluptatibus ducimus totam dignissimos
+                  ratione quidem! Et!
+                </Card.Text>
+                <Button variant="primary">Go somewhere</Button>
+              </Card.Body>
+            </Card>
+          </Carousel.Item>{" "}
+          <Carousel.Item interval={2000}>
+            <Card
+              style={{ width: "100%", height: "300px" }}
+              className="card-style"
+            >
+              <Card.Img
+                variant="left"
+                src="https://i.ibb.co/0Jv0WP5/ezgif-com-gif-maker.webp"
+                style={{ maxWidth: "300px", maxHeight: "300px" }}
+              />
+              <Card.Body>
+                <Card.Title className="fredokaB">Card Title</Card.Title>
+                <Card.Text className="fredokaB">
+                  Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+                  Dolores atque quos nostrum necessitatibus ipsam rem inventore
+                  reiciendis, distinctio eveniet esse quasi id aspernatur fugit
+                  dignissimos laboriosam quibusdam vel voluptates? Blanditiis.
+                </Card.Text>
+                <Button variant="primary">Go somewhere</Button>
+              </Card.Body>
+            </Card>
+          </Carousel.Item>
+        </Carousel>
+      ) : (
+        <></>
+      )}
     </div>
   );
 };
