a1d0ef49feeecacc6c1f84ad9aed8cf437fb5442
Abdurrahman al djufry <Abdurrahmanaldjufry@gmail.com>
2023-12-31 09:07:07 +0700

    fixing netlify bug

--- src/ab-component/Footer.jsx
+++ src/ab-component/Footer.jsx
@@ -1,12 +1,12 @@
 import React from "react";
-import { Container, Row, Col } from "react-bootstrap";
+import { Row, Col } from "react-bootstrap";
 
 const Footer = () => {
   return (
-      <Row className="footer-container">
-        <Col></Col>
-        <Col></Col>
-      </Row>
+    <Row className="footer-container">
+      <Col></Col>
+      <Col></Col>
+    </Row>
   );
 };
 
